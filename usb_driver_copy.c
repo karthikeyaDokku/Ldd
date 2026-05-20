e55855e876b618a407b4a64873fd1127505f0c77
karthikeya <karthikeya2601@gmail.com>
2026-05-20 11:02:41 +0530

    Usb driver skeleton

---
+++ usb_driver_copy.c
@@ -0,0 +1,20 @@
+#include<linux/usb.h>
+#include<linux/module.h>
+
+static int __init usb_skel_init(void){
+	int result;
+	printk(KERN_INFO "Module is initialised\n");
+	return 0;
+}
+
+module_init(usb_skel_init);
+
+static void __exit usb_skel_exit(void){
+	printk(KERN_INFO "Module is exited\n");
+}
+
+module_exit(usb_skel_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Karthikeya");
+
