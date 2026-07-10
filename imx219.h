b2d9b8325354467f20742831cba4423ad4670e8c
karthikeyaDokku <karthikeya2601@gmail.com>
2026-07-10 22:30:39 +0530

    Add custom IMX219 driver and basic device tree overlay

---
+++ imx219.h
@@ -0,0 +1,43 @@
+#ifndef _MY_IMX219_H_
+#define _MY_IMX219_H_
+
+#define IMX219_NAME            "imx219_basic"
+
+/* chip id */
+#define IMX219_REG_CHIP_ID_H   0x0000
+#define IMX219_REG_CHIP_ID_L   0x0001
+#define IMX219_CHIP_ID         0x0219
+
+/* stream */
+#define IMX219_REG_MODE_SELECT 0x0100
+#define IMX219_STREAM_OFF      0x00
+#define IMX219_STREAM_ON       0x01
+
+/* gain */
+#define IMX219_ANALOG_GAIN     0x0157
+
+/* exposure */
+#define IMX219_EXPOSURE_H      0x015A
+#define IMX219_EXPOSURE_L      0x015B
+
+/* flip */
+#define IMX219_ORIENTATION     0x0172
+
+/* test pattern */
+#define IMX219_TEST_PATTERN    0x0600
+
+/* ---- NEW: power / clock / reset ---- */
+
+/* External clock the sensor needs on XCLK (Raspberry Pi CSI connector) */
+#define IMX219_XCLK_FREQ        24000000
+
+/* Delays taken from the vendor/mainline driver timing diagram */
+#define IMX219_XCLK_MIN_DELAY_US    6200
+#define IMX219_XCLK_DELAY_RANGE_US  1000
+#define IMX219_RESET_MIN_DELAY_US   100
+#define IMX219_RESET_DELAY_RANGE_US 100
+
+/* Number of supplies the sensor needs powered before reset/clock */
+#define IMX219_NUM_SUPPLIES 3
+
+#endif
